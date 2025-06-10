69add90b416c72ede78f03bbec345f438eaaeead
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-06-10 22:55:25 +0530

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
 import { routeTree } from "./routing/routeTree.js";
+import { store } from "./store/store.js";
 
 const queryClient = new QueryClient();
-const router = createRouter({routeTree})
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}/>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
