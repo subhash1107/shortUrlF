c80357d8444f3be9ae26429bb88b2e4203d16463
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-06-10 21:02:32 +0530

    created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from '@tanstack/react-router'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className=''>
+      <Navbar/>
       <Outlet/>
     </div>
   )
